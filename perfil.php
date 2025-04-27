90e49707fb753d8e2d0c4a7200ee5a5ac32a8a9b
MarioJunior2 <mario.junior2@alunos.sc.senac.br>
2025-04-27 01:25:00 -0300

    3 Horas aba de notific pronta

---
+++ perfil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil - TydraPI</title>
+    <style>
+        .sidebar {
+            position: fixed;
+        }
+    </style>
+    <?php include 'header.php' ?>
+</head>
+<body>
+    <?php include 'sidebar.php' ?>
+</body>
+</html>
